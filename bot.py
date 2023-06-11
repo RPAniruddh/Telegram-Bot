ac22b114bbb94c377aefd35d84adcf00a765aeed
Aniruddh R Panicker <127539182+RPAniruddh@users.noreply.github.com>
2023-06-11 21:53:29 +0530

    Update bot.py
    Made some changes to the reply

--- bot.py
+++ bot.py
@@ -7,10 +7,10 @@ bot = telebot.TeleBot(BOT_TOKEN)
 
 @bot.message_handler(commands=['start', 'hello'])
 def send_welcome(message):
-    bot.reply_to(message, "Hello butterfly, miss me?")
+    bot.reply_to(message, "Hello there")
 
 @bot.message_handler(func=lambda msg: True)
 def echo_all(message):
-    bot.reply_to(message, "Love You")
+    bot.reply_to(message, "Not valid")
 
 bot.infinity_polling()
